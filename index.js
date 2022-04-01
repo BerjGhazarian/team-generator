84b02568b9b3a05d526caf198089dd514800add4
berj ghazarian <ghazarian_berj@hotmail.com>
2022-04-01 16:10:07 -0700

    finished prompts

--- index.js
+++ index.js
@@ -61,3 +61,70 @@ function init() {
 
             })
     }
+    function engineerCreate() {
+        inquirer.prompt([
+            {
+                 type: 'input',
+                 name: 'name',
+                 message: 'What is the Engineer name?',
+                },
+                {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Engineer id?',
+                },
+                {
+                 type: 'input',
+                 name: 'email',
+                 message: 'What is the Engineer email?',
+                },
+                {
+                 type: 'input',
+                 name: 'github',
+                 message: 'What is the Github username?',
+            }
+        ])
+        .then(({name, id, email, github}) => {
+            const engineer = new Engineer(name, id, email, github);
+            teamMemberHtmlArr.push(generateEngineerCard(engineer));
+            mainMenu();
+        });
+    }
+    function internCreate() {
+        inquirer.prompt([
+            {
+                 type: 'input',
+                 name: 'name',
+                 message: 'What is the Intern name?',
+                },
+                {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Intern id?',
+                },
+                {
+                 type: 'input',
+                 name: 'email',
+                 message: 'What is your email?',
+                },
+                {
+                 type: 'input',
+                 name: 'school',
+                 message: 'What school does the intern go to?',
+            }
+        ])
+        .then(({name, id, email, school}) => {
+            const intern = new Intern(name, id, email, school);
+            teamMemberHtmlArr.push(generateInternCard(intern));
+            mainMenu();
+        });
+    }
+    function generateHtml() {
+        fs.writeFile("./dist/index.html", baseHtml(teamMemberHtmlArr), (err) => {
+            err ? console.log(err) : console.log("Generated HTML file!")
+        })
+    }
+
+    managerCreate();
+}
+init();
