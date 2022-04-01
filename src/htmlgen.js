3e833cb4c34e1a6a0de5551705d0752b3e580c49
berj ghazarian <ghazarian_berj@hotmail.com>
2022-04-01 16:12:45 -0700

    added base html to gen

--- src/htmlgen.js
+++ src/htmlgen.js
@@ -44,3 +44,38 @@ const generateInternCard = (intern) => {
   </div>
     `
 };
+const baseHtml = (htmlArr) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Software Engineering Team Profile</title>
+</head>
+<body>
+    <header>
+    <div class="jumbotron">
+    <h1 class="display-4">My Team</h1>
+    <p class="lead"></p>
+    <hr class="my-4">
+  </div>
+    </header>
+    <div id="container">
+    <div class="card-deck">
+        ${htmlArr.join(" ")}
+    </div>
+    </div>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
+`
+
+module.exports = {
+    generateManagerCard,
+    generateEngineerCard,
+    generateInternCard,
+    baseHtml,
+}
